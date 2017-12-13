c39663809bf187fcd4f1e489bc23b5de0a78d2ee
anasanz02 <34066708+anasanz02@users.noreply.github.com>
2017-12-13 17:40:15 -0600

    cambio en registro
    campo tipo aspirante agregado

--- AppDica/MyMainApp/Registro.aspx.designer.cs
+++ AppDica/MyMainApp/Registro.aspx.designer.cs
@@ -49,6 +49,24 @@ namespace MyMainApp {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// Control DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
         /// Control Label2.
         /// </summary>
         /// <remarks>
